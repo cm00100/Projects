713f78478021f0367f2474a593c7f39e8a68e021
cm00100 <cyn.mart@outlook.com>
2019-12-06 10:29:31 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link  href="css/style.css" rel="stylesheet" type="text/css" />
+        <title>CM Projects</title>
+        <meta charset="utf-8" />
+    </head>
+
+    <body>
+        
+          <h1>Hello World</h1>  
+  
+    </body>
+</html>
